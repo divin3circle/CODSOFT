ba002548cd4686084c50b568d3ec4ad9f0413cce
Sylus Abel <omutelema.silas21@students.dkut.ac.ke>
2023-10-05 13:06:16 +0300

    value-section

--- job-post/src/jobs/Jobs.jsx
+++ job-post/src/jobs/Jobs.jsx
@@ -2,40 +2,47 @@ import React from "react";
 /* import { BiFiveTime } from "react-icons/bi" */
 import { FaBusinessTime } from "react-icons/fa";
 import logo1 from "../assets/images/google.jpg";
+import { jobs } from "../assets/jobsData";
 const Jobs = () => {
   return (
     <div>
       <div className="jobContainer flex flex-wrap justify-center items-center gap-10">
-        <div className="w-[250px] p-[20px] bg-card hover:bg-accent cursor-pointer ease-in duration-200 singleJob group group/items rounded-lg shadow-md shadow-accent hover:scale-105 ">
-          <span className="flex justify-center items-center gap-4">
-            <h1 className=" group-hover:text-background font-semibold text-[16px]">
-              Web Developer
-            </h1>
-            <span className="text-card/90 font-400 flex justify-center items-center gap-2">
-              <FaBusinessTime />
-              Now
-            </span>
-          </span>
-          <h6 className="text-text font-400">Canada</h6>
-          <p className="text-[13px] text-text py-[20px] border-t-[2px] group-hover:text-background mt-[20px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            suscipit ullam nemo quo itaque, temporibus ipsam ratione consectetur
-            hic nihil accusamus explicabo dignissimos error distinctio esse,
-            adipisci harum, amet neque?
-          </p>
+        {jobs.map((job, i) => {
+          return (
+            <div
+              key={job.id}
+              className="w-[250px] min-h-[380px] p-[20px] bg-card hover:bg-accent cursor-pointer ease-in duration-200 singleJob group group/items rounded-lg shadow-md shadow-accent hover:scale-105 "
+            >
+              <span className="flex justify-center items-center gap-4">
+                <h1 className=" group-hover:text-background font-semibold text-[16px]">
+                  {job.title}
+                </h1>
+                <span className="text-card/90 font-400 flex justify-center items-center gap-2">
+                  <FaBusinessTime />
+                  {job.time}
+                </span>
+              </span>
+              <h6 className="text-text font-400">{job.location}</h6>
+              <p className="text-[13px] text-text py-[20px] border-t-[2px] group-hover:text-background mt-[20px]">
+                {job.desc}
+              </p>
 
-          <div className="company flex items-center gap-2">
-            <img
-              src={logo1}
-              alt="company_logo"
-              className="w-[15%] h-[15%] rounded-full "
-            />
-            <span className="text-[14px] block py-[1rem]">Google</span>
-          </div>
-          <button className="rounded-[10px] border-[2px] block p-[10px] w-full text-[14px] font-semibold hover:bg-background ease-in duration-150 ">
-            Get this job
-          </button>
-        </div>
+              <div className="company flex items-center gap-2">
+                <img
+                  src={job.image}
+                  alt="company_logo"
+                  className="w-[15%] h-[15%] object-fill rounded-full "
+                />
+                <span className="text-[14px] block py-[1rem]">
+                  {job.company}
+                </span>
+              </div>
+              <button className="rounded-[10px] border-[2px] block p-[10px] w-full text-[14px] font-semibold hover:bg-background ease-in duration-150 ">
+                Get this job
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- job-post/src/value/Value.jsx
+++ job-post/src/value/Value.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import simple from "../assets/images/simple.jpg";
+import home from "../assets/images/home.jpg";
+import work from "../assets/images/work.jpg";
 
 const Value = () => {
-  return <div>Value</div>;
+  return (
+    <div className="mt-[6rem] mb-[4rem]">
+      <h1 className="w-[400px] font-semibold block py-[2rem] text-[25px]">
+        Let's help you find a job at the comfort of your home
+      </h1>
+      <div className="grid grid-cols-3 gap-[10rem] place-content-center">
+        <div className="singleGrid p-[1.5rem] rounded-xl hover:bg-accent cursor-pointer hover:text-text ease-in duration-300">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-inherit-hover h-[40px] w-[40px] flex items-center justify-center">
+              <img src={simple} alt="" className="rounded-full" />
+            </div>
+            <span className=" font-semibold">Simplicity</span>
+          </div>
+          <p className=" opacity-70 font-semibold text-[15px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </div>
+        <div className="singleGrid p-[1.5rem] rounded-xl hover:bg-accent cursor-pointer hover:text-text ease-in duration-300">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-inherit-hover h-[40px] w-[40px] flex items-center justify-center">
+              <img src={home} alt="" className="rounded-full" />
+            </div>
+            <span className=" font-semibold">Directly from home</span>
+          </div>
+          <p className=" opacity-70 font-semibold text-[15px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </div>
+        <div className="singleGrid p-[1.5rem] rounded-xl hover:bg-accent cursor-pointer hover:text-text ease-in duration-300">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-inherit-hover h-[40px] w-[40px] flex items-center justify-center">
+              <img src={work} alt="" className="rounded-full" />
+            </div>
+            <span className=" font-semibold">100% Free</span>
+          </div>
+          <p className=" opacity-70 font-semibold text-[15px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Value;
